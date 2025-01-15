f6b5dd734c9e2c8631d9de5da9431448cf7edb6a
s07k <shubham3431kumar@gmail.com>
2025-01-15 21:53:49 +0530

    added forgot password page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import LoginPage from "./app/login/page";
 import SignupPage from "./app/signup/page";
 import Page from "./app/dashboard/page";
+import ForgotPasswordPage from "./app/forgotPassword/page";
 import ErrorPage from "./components/ui/ErrorPage";
 import { BrowserRouter, Route, Routes } from "react-router";
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/sign-up" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/" element={<Page />} />
         <Route path="/*" element={<ErrorPage />}  />
       </Routes>
---
+++ src/components/forgot-password.tsx
@@ -0,0 +1,49 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import voiceMotion from "../assets/voice-motion.mp4"
+
+export function ForgotPassword({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-start">
+                <h1 className="text-2xl font-bold">Reset password</h1>
+                <p className="text-muted-foreground">
+                  Enter your email to receive a password reset link
+                </p>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <Button type="submit" className="w-full">
+                Send Reset Link
+              </Button>
+            </div>
+          </form>
+          <div className="relative hidden bg-muted md:block">
+            <video src={voiceMotion} autoPlay loop muted className="absolute inset-0 h-full w-full object-cover" />
+          </div>
+        </CardContent>
+      </Card>
+      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div>
+    </div>
+  )
+}
